20f4c977942eebb50aad84d1f8b99404bd943f8e
Frank Scholten <frank@frankscholten.nl>
2011-12-15 09:42:41 +0100

    Processed review comments. Now uses Weight and Scorer instead of TopDocs. Removed Filter from configuration. Upgraded to Lucene 3.5.0

--- integration/src/main/java/org/apache/mahout/text/LuceneIndexToSequenceFiles.java
+++ integration/src/main/java/org/apache/mahout/text/LuceneIndexToSequenceFiles.java
@@ -24,9 +24,7 @@ import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.search.Filter;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.slf4j.Logger;
@@ -34,82 +32,90 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+import static org.apache.lucene.search.DocIdSetIterator.NO_MORE_DOCS;
+
 /**
  * Generates a sequence file from a Lucene index with a specified id field as the key and a content field as the value.
  * Configure this class with a {@link LuceneIndexToSequenceFilesConfiguration} bean.
  */
 public class LuceneIndexToSequenceFiles {
 
-  private static final Logger log = LoggerFactory.getLogger(LuceneIndexToSequenceFiles.class);
+    private static final Logger log = LoggerFactory.getLogger(LuceneIndexToSequenceFiles.class);
 
-  /**
-   * Generates a sequence files from a Lucene index via the given {@link LuceneIndexToSequenceFilesConfiguration}
-   *
-   * @param lucene2seqConf configuration bean
-   * @throws java.io.IOException if index cannot be opened or sequence file could not be written
-   */
-  public void run(LuceneIndexToSequenceFilesConfiguration lucene2seqConf) throws IOException {
-    Directory directory = FSDirectory.open(lucene2seqConf.getIndexLocation());
-    IndexReader reader = IndexReader.open(directory, true);
-    IndexSearcher searcher = new IndexSearcher(reader);
-    Configuration configuration = lucene2seqConf.getConfiguration();
-    FileSystem fileSystem = FileSystem.get(configuration);
-    SequenceFile.Writer sequenceFileWriter = new SequenceFile.Writer(fileSystem, configuration, lucene2seqConf.getSequenceFilesOutputPath(), Text.class, Text.class);
+    /**
+     * Generates a sequence files from a Lucene index via the given {@link LuceneIndexToSequenceFilesConfiguration}
+     *
+     * @param lucene2seqConf configuration bean
+     * @throws java.io.IOException if index cannot be opened or sequence file could not be written
+     */
+    public void run(LuceneIndexToSequenceFilesConfiguration lucene2seqConf) throws IOException {
+        Directory directory = FSDirectory.open(lucene2seqConf.getIndexLocation());
+        IndexReader reader = IndexReader.open(directory, true);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        Configuration configuration = lucene2seqConf.getConfiguration();
+        FileSystem fileSystem = FileSystem.get(configuration);
+        SequenceFile.Writer sequenceFileWriter = new SequenceFile.Writer(fileSystem, configuration, lucene2seqConf.getSequenceFilesOutputPath(), Text.class, Text.class);
 
-    Text key = new Text();
-    Text value = new Text();
+        Text key = new Text();
+        Text value = new Text();
 
-    TopDocs topDocs = search(searcher, lucene2seqConf);
+        Weight weight = lucene2seqConf.getQuery().createWeight(searcher);
+        Scorer scorer = weight.scorer(reader, true, false);
 
-    int processedDocs = 0;
+        if (scorer != null) {
+            int processedDocs = 0;
+            int docId;
 
-    for (int i = 0; processedDocs != lucene2seqConf.getMaxHits() && i < topDocs.scoreDocs.length; i++) {
-      Document doc = searcher.doc(topDocs.scoreDocs[i].doc);
-      String idValue = doc.get(lucene2seqConf.getIdField());
+            while ((docId = scorer.nextDoc()) != NO_MORE_DOCS && processedDocs < lucene2seqConf.getMaxHits()) {
+                Document doc = reader.document(docId, lucene2seqConf.getFieldSelector());
 
-        String field = lucene2seqConf.getField();
-        String fieldValue = doc.get(field);
-        if (fieldValue == null) {
-            log.info("Null value for document {} field {}", idValue, field);
-            continue;
+                String idValue = doc.get(lucene2seqConf.getIdField());
+                String field = lucene2seqConf.getField();
+                String fieldValue = doc.get(field);
+
+                if (fieldValue == null) {
+                    log.info("Null value for document {} field {}", idValue, field);
+                    continue;
+                }
+
+                StringBuilder fieldValueBuilder = new StringBuilder(fieldValue);
+                if (lucene2seqConf.getExtraFields() != null && !lucene2seqConf.getExtraFields().isEmpty()) {
+                    for (String extraField : lucene2seqConf.getExtraFields()) {
+                        String extraFieldValue = doc.get(extraField);
+                        fieldValueBuilder.append(" ").append(extraFieldValue);
+                    }
+                }
+
+                String concatenatedFieldValue = fieldValueBuilder.toString();
+
+                if (isBlank(idValue) || isBlank(concatenatedFieldValue)) {
+                    continue;
+                }
+
+                key.set(idValue);
+                value.set(concatenatedFieldValue);
+
+                sequenceFileWriter.append(key, value);
+
+                processedDocs++;
+            }
+
+            log.info("Wrote " + processedDocs + " documents");
+        } else {
+            Closeables.closeQuietly(sequenceFileWriter);
+            directory.close();
+            searcher.close();
+            reader.close();
+            throw new RuntimeException("Could not write sequence files. Could not create scorer");
         }
-        StringBuilder fieldValueBuilder = new StringBuilder(fieldValue);
-      if (lucene2seqConf.getExtraFields() != null && !lucene2seqConf.getExtraFields().isEmpty()) {
-        for (String extraField : lucene2seqConf.getExtraFields()) {
-          String extraFieldValue = doc.get(extraField);
-          fieldValueBuilder.append(" ").append(extraFieldValue);
-        }
-      }
 
-      String concatenatedFieldValue = fieldValueBuilder.toString();
-
-      if (idValue == null || idValue.equals("") || concatenatedFieldValue == null || concatenatedFieldValue.equals("")) {
-        continue;
-      }
-
-      key.set(idValue);
-      value.set(concatenatedFieldValue);
-
-      sequenceFileWriter.append(key, value);
-
-      processedDocs++;
+        Closeables.closeQuietly(sequenceFileWriter);
+        directory.close();
+        searcher.close();
+        reader.close();
     }
 
-    log.info("Wrote " + processedDocs + "/" + topDocs.totalHits + " documents");
-
-    Closeables.closeQuietly(sequenceFileWriter);
-
-    directory.close();
-    reader.close();
-    searcher.close();
-  }
-
-  private TopDocs search(IndexSearcher searcher, LuceneIndexToSequenceFilesConfiguration lucene2SeqConf) throws IOException {
-    Filter filter = lucene2SeqConf.getFilter();
-    if (filter != null) {
-      return searcher.search(lucene2SeqConf.getQuery(), filter, Integer.MAX_VALUE);
-    } else {
-      return searcher.search(lucene2SeqConf.getQuery(), Integer.MAX_VALUE);
+    private boolean isBlank(String idValue) {
+        return idValue == null || idValue.equals("");
     }
-  }
 }
