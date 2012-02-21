6128b57886562a0b48505f0d091e79d0c18b9c58
Frank Scholten <frank@frankscholten.nl>
2012-02-21 16:19:41 +0100

    Added test for numeric fields.

---
+++ integration/src/test/java/org/apache/mahout/text/doc/NumericFieldDocument.java
@@ -0,0 +1,34 @@
+package org.apache.mahout.text.doc;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.NumericField;
+
+/**
+ * Document with numeric field.
+ */
+public class NumericFieldDocument extends SingleFieldDocument {
+
+  public static final String NUMERIC_FIELD = "numeric";
+
+  private int numericField;
+
+  public NumericFieldDocument(String id, String field, int numericField) {
+    super(id, field);
+    this.numericField = numericField;
+  }
+
+  public Document asLuceneDocument() {
+    Document document = new Document();
+
+    document.add(new Field(ID_FIELD, getId(), Field.Store.YES, Field.Index.NO));
+    document.add(new Field(FIELD, getField(), Field.Store.YES, Field.Index.ANALYZED));
+    document.add(new NumericField(NUMERIC_FIELD, Field.Store.YES, false).setIntValue(numericField));
+
+    return document;
+  }
+
+  public int getNumericField() {
+    return numericField;
+  }
+}
