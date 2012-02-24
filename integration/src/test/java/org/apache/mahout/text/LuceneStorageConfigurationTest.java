6e94b96b1f4d080a72b56e0d7c344b6f6d9d9293
Frank Scholten <frank@frankscholten.nl>
2012-02-24 17:57:06 +0100

    Added test for missing serialization in Configuration

--- integration/src/test/java/org/apache/mahout/text/LuceneStorageConfigurationTest.java
+++ integration/src/test/java/org/apache/mahout/text/LuceneStorageConfigurationTest.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
@@ -21,5 +23,10 @@ public class LuceneStorageConfigurationTest {
     LuceneStorageConfiguration deserializedConf = new LuceneStorageConfiguration(serializedConf);
 
     assertEquals(luceneStorageConf, deserializedConf);
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSerialization_notSerialized() throws IOException {
+    new LuceneStorageConfiguration(new Configuration());
   }
 }
